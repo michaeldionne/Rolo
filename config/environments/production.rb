7ee4ce5df2452c570cb284eb6851f025bb04d740
Michael Dionne <dionnemj1@gmail.com>
2013-09-28 16:37:56 -0700

    Added and configured Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Followup::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Required for Devise
+  #Note to set this to actual hostname
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
